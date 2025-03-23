6e3cf6f379bbfb6d6c67378d760f07ca61360ae1
Natalee Nefedova <nefedova.nk@gmail.com>
2025-03-23 14:34:51 +0300

    step5

--- brain_games/brain_even_game.py
+++ brain_games/brain_even_game.py
@@ -1,33 +1,30 @@
 from random import randint
-from cli import welcome_user
+import prompt
 
 def even_game(): 
+
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print("Hello, " + name + "!")
     print('Answer "yes" if the number is even, otherwise answer "no".')
     count = 0
     success = 0
     while count < 3:
-        random_number = randint(1, 20)
+        random_number = randint(1, 10)
         result = ''        
         if random_number % 2 == 0:
             result = 'yes'
         else: 
             result = 'no'    
         print("Question: " + str(random_number))
-        answer = ''
-        while answer == '':
-            print("Your answer: ", end='')
-            answer = input()       
+        answer = prompt.string("Your answer: ") 
         if answer != result:
             print("'" + answer + "'" + ' is wrong answer ;(. Correct answer was ' + "'" + result + "'")
-            print("Let's try again, " + "name" + "!")
+            print("Let's try again, " + name + "!")
             break
         else:
             print('Correct!')
             count += 1
-            success += 1
-        
+            success += 1        
     if success == 3:
-        print("Congratulations " + "name" + "!") 
-
-welcome_user()
-even_game()
+        print("Congratulations, " + name + "!") 
--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,13 +1,7 @@
-# import prompt
+import prompt
 
 
 def welcome_user():
     
-    # name = prompt.string('May I have your name? ')
-    # print("Hello, " + name + "!")
-
-    name = ''
-    while name == '':
-        print('May I have your name? ', end='')
-        name = input()
-        print("Hello, " + name + "!")
+    name = prompt.string('May I have your name? ')
+    print("Hello, " + name + "!")
